8f6fe44461c9eec4b17168b60c1215898c7446e7
Brian Malsan <bfmalsan@gmail.com>
2019-04-20 15:00:04 -0400

    Set up project
    Created project environment in Xcode 10

---
+++ Minesweeper/Minesweeper/GameScene.swift
@@ -0,0 +1,24 @@
+//
+//  GameScene.swift
+//  Minesweeper
+//
+//  Created by Brian Malsan on 4/20/19.
+//  Copyright © 2019 Brian Malsan. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+    
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
